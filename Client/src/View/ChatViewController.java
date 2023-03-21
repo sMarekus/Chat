eb815d04c98f9589355a5488d305c80497f9b3fb
mtezxx <118538867+mtezxx@users.noreply.github.com>
2023-03-21 13:45:33 +0100

    controllers

---
+++ Client/src/View/ChatViewController.java
@@ -0,0 +1,31 @@
+package View;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+  public class ChatViewController {
+
+    @FXML
+    private TextArea chatField;
+
+    @FXML
+    private TextArea messageField;
+
+    @FXML
+    private TextArea userField;
+
+    @FXML
+    void onSend(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onShow(ActionEvent event) {
+
+    }
+
+  }
+
+
---
+++ Client/src/View/LoginViewController.java
@@ -0,0 +1,17 @@
+package View;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginViewController {
+
+  @FXML
+  private TextField usernameField;
+
+  @FXML
+  void onLogIn(ActionEvent event) {
+
+  }
+}
+
