c55919799fa7c5160c4974183d10fb3aab3e71c9
mtezxx <118538867+mtezxx@users.noreply.github.com>
2023-03-24 11:53:53 +0100

    up

--- Client/src/Model/User.java
+++ Client/src/Model/User.java
@@ -3,25 +3,22 @@ package Model;
 public class User
 {
  private final String userName;
- private final String password;
+
 
   public User(String userName)
   {
     this.userName = userName;
-    this.password = password;
+
   }
 
   public String getUserName(){
     return userName;
   }
 
-  public String getPassword(){
-    return password;
-  }
+
 
   @Override public String toString()
   {
-    return "User{" + "userName='" + userName + '\'' + ", password='" + password
-        + '\'' + '}';
+    return "User{" + "userName='" + userName + '\'' ;
   }
 }
